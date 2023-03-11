1814580a6c5dc2606942ca1599874b3b1e48b5fe
Miguel Gonzalez Otoya <miguel_otoya@outlook.com>
2023-03-11 02:09:43 -0600

    Seeding with ef core

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,5 +12,44 @@ namespace Country_Information.Data
         }
          public DbSet<Country> Countries { get; set; }
          public DbSet<Continent> Continents { get; set; }
+
+         protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Continent>().HasData(
+                new Continent
+                {
+                    id = 1,
+                    name = "America",
+                    description = "The only one who aliens invade"
+                },
+                new Continent
+                {
+                    id = 2,
+                    name = "Europe",
+                    description = "The third most populous continent"
+                },
+                new Continent
+                {
+                    id = 3,
+                    name = "Asia",
+                    description = "The largest continent"
+                },
+                new Continent
+                {
+                    id = 4,
+                    name = "Oceania",
+                    description = "kangaroos"
+                },
+                new Continent
+                {
+                    id = 5,
+                    name = "Africa",
+                    description = "The N word"
+                });
+                
+        }
+
     }
+
+    
 }
---
+++ Migrations/20230311080520_SeedMigration.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Country_Information.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "continents",
+                columns: new[] { "id", "description", "name" },
+                values: new object[,]
+                {
+                    { 1, "The only one who aliens invade", "America" },
+                    { 2, "The third most populous continent", "Europe" },
+                    { 3, "The largest continent", "Asia" },
+                    { 4, "kangaroos", "Oceania" },
+                    { 5, "The N word", "Africa" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "continents",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "continents",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "continents",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "continents",
+                keyColumn: "id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "continents",
+                keyColumn: "id",
+                keyValue: 5);
+        }
+    }
+}
